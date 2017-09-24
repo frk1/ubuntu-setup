27819821bd90fa44aaf44170d947715cf6b5bdb7
frk <frk@haze-productions.com>
2017-09-24 16:13:47 +0200

    :pencil: sshd_config

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -212,63 +212,36 @@ runuser -l $SCRIPT_USERNAME -c 'zsh -s' <<'EOF'
   cargo install --git https://github.com/ogham/exa.git
 EOF
 
-chown -Rh $SCRIPT_USERNAME:$SCRIPT_USERNAME /home/$SCRIPT_USERNAME
+chown -Rh $SCRIPT_USERNAME /home/$SCRIPT_USERNAME
 
 printf -- "ok\n"
 printf -- "- Configuring openSSH server..."
 
-cat <<EOF > /etc/ssh/sshd_config
-Port 22
-Protocol 2
-
+cat <<'EOF' > /etc/ssh/sshd_config
 HostKey /etc/ssh/ssh_host_ed25519_key
 HostKey /etc/ssh/ssh_host_rsa_key
-UsePrivilegeSeparation sandbox
-
-KeyRegenerationInterval 3600
-ServerKeyBits 1024
-
-SyslogFacility AUTH
-LogLevel VERBOSE
-
-LoginGraceTime 120
-PermitRootLogin no
-StrictModes yes
-
-RSAAuthentication yes
-PubkeyAuthentication yes
-
-IgnoreRhosts yes
-RhostsRSAAuthentication no
-HostbasedAuthentication no
-PermitEmptyPasswords no
-ChallengeResponseAuthentication no
-PasswordAuthentication no
-
-X11Forwarding no
-X11DisplayOffset 10
-PrintMotd no
-PrintLastLog yes
-TCPKeepAlive yes
-
-AcceptEnv LANG LC_*
-Subsystem sftp /usr/lib/openssh/sftp-server
 
 KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256
-Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
 MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+
+AuthenticationMethods publickey
+LogLevel VERBOSE
+Subsystem sftp /usr/lib/ssh/sftp-server -f AUTHPRIV -l INFO
+PermitRootLogin No
+UsePrivilegeSeparation sandbox
 EOF
 
 cat <<EOF > /etc/ssh/ssh_config
 HashKnownHosts yes
 HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa,ecdsa-sha2-nistp521-cert-v01@openssh.com,ecdsa-sha2-nistp384-cert-v01@openssh.com,ecdsa-sha2-nistp256-cert-v01@openssh.com,ecdsa-sha2-nistp521,ecdsa-sha2-nistp384,ecdsa-sha2-nistp256
-KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp256,ecdh-sha2-nistp384,diffie-hellman-group-exchange-sha256
+KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256
 MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
 Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
 EOF
 
 if [ -z "$SCRIPT_SSH_KEY" ]; then
-  sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+  sed -i "/AuthenticationMethods/s/publickey/publickey password/g" /etc/ssh/sshd_config
 fi
 
 cd /etc/ssh
