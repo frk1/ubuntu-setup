6dc347209ff7d494bdf20066101703fddf3ee6e1
ReactiioN <reactiion@haze-productions.com>
2019-10-31 03:18:00 +0100

    🚀 Updated version script to handle both installer

--- versions.sh
+++ versions.sh
@@ -17,12 +17,17 @@ export VERSION_LIBRESSL=$(github_get_latest_tag libressl-portable portable)
 export VERSION_CMAKE=$(github_get_latest_tag Kitware CMake)
 export VERSION_CURL=$(github_get_latest_tag curl curl)
 
-sed -i -r -e "s/VERSION_GIT=[0-9\\.]+/VERSION_GIT=$VERSION_GIT/g"                \
-          -e "s/VERSION_TMUX=[0-9\\.]+/VERSION_TMUX=$VERSION_TMUX/g"             \
-          -e "s/VERSION_VIM=[0-9\\.]+/VERSION_VIM=$VERSION_VIM/g"                \
-          -e "s/VERSION_ZSH=[0-9\\.]+/VERSION_ZSH=$VERSION_ZSH/g"                \
-          -e "s/VERSION_FASD=[0-9\\.]+/VERSION_FASD=$VERSION_FASD/g"             \
-          -e "s/VERSION_LIBRESSL=[0-9\\.]+/VERSION_LIBRESSL=$VERSION_LIBRESSL/g" \
-          -e "s/VERSION_CMAKE=[0-9\\.]+/VERSION_CMAKE=$VERSION_CMAKE/g"          \
-          -e "s/VERSION_CURL=[0-9\\.]+/VERSION_CURL=$VERSION_CURL/g"             \
-              ubuntu-setup.sh
+updateVersions() {
+  sed -i -r -e "s/VERSION_GIT=[0-9\\.]+/VERSION_GIT=$VERSION_GIT/g"                \
+            -e "s/VERSION_TMUX=[0-9\\.]+/VERSION_TMUX=$VERSION_TMUX/g"             \
+            -e "s/VERSION_VIM=[0-9\\.]+/VERSION_VIM=$VERSION_VIM/g"                \
+            -e "s/VERSION_ZSH=[0-9\\.]+/VERSION_ZSH=$VERSION_ZSH/g"                \
+            -e "s/VERSION_FASD=[0-9\\.]+/VERSION_FASD=$VERSION_FASD/g"             \
+            -e "s/VERSION_LIBRESSL=[0-9\\.]+/VERSION_LIBRESSL=$VERSION_LIBRESSL/g" \
+            -e "s/VERSION_CMAKE=[0-9\\.]+/VERSION_CMAKE=$VERSION_CMAKE/g"          \
+            -e "s/VERSION_CURL=[0-9\\.]+/VERSION_CURL=$VERSION_CURL/g"             \
+                $1
+}
+
+updateVersions "ubuntu-setup-1604.sh"
+updateVersions "ubuntu-setup-1804.sh"
